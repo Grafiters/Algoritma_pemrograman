51f256d15b7b381b10212a2c29148e1cb77147a7
saipul <bayugrafit@gmail.com>
2018-03-24 17:56:47 +0700

    add new variabel

--- PAP4/tugas/main.c
+++ PAP4/tugas/main.c
@@ -3,7 +3,7 @@
 int arr1[] = { 3, 5, 38, 0, 44, 47 };
 int arr2[] = { 3, 0, 44, 38, 0, 5, 47 };
 int arr3[] = { 10, 5, 21, 30, 27 };
-int arr4[] = { 15, 24, 20, 2, 36 };
+int arr4[] = { 15, 24, 32, 20, 30 };
 
 int main()
 {
@@ -14,9 +14,12 @@ int main()
     puts("\n");
     puts("====== Tugas 2 Selection Short 1 ======");
     selection_short1(arr3,5);
-    cetak_array(arr3,5);
     puts("\n");
     puts("====== Tugas 3 Selection Short 2 ======");
     selection_short2(arr4, 5);
+    puts("\n");
+    puts("====== Tuags 4 Get Median ======");
+    cetak_array(arr4, 5);
+    get_median(arr4,5);
     return 0;
 }

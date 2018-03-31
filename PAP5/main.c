00e7e0137da6ccceee8df71b2a650f0dc2e922ae
saipul <bayugrafit@gmail.com>
2018-03-31 17:34:08 +0700

    edited main

--- PAP5/main.c
+++ PAP5/main.c
@@ -1,7 +1,15 @@
 #include "pustaka.h"
 
 int data[] = {12, 14, 20, 15, 22, 25, 18, 10, 27};
-int angka;
+int list1[] = {1, 2, 3, 4, 5};
+int list2[] = {5, 4, 3, 2, 1};
+int list3[] = {6, 7, 8, 9, 10};
+int list4[] = {10, 9, 8, 7, 6};
+
+int list5[] = {9, 12, 43, 13, 15};
+int list6[] = {12, 4, 19, 9, 22, 1};
+
+int angka, a, b;
 
 int main()
 {
@@ -9,5 +17,17 @@ int main()
     puts("");
     printf("iput nilai yang dicari : "); scanf("%d", &angka);
     printf("apakah %d nilai ada pada array %d", angka, sequent_search(angka, data, 9));
+
+    puts("");
+    cetakarray(list5,5);
+    puts("");
+    min_max(list5, 5, &a, &b);
+    printf("a = %d b = %d", a,b);
+    puts("");
+    cetakarray(list6, 6);
+    puts("");
+    printf("Jarak antara nilai terkecil dan terbesar adalah %d", min_max_gap(list6, 6));
+    puts("");
+    printf("at_least1 %s ",at_least('r', 3,"Larry Page"));
     return 0;
 }

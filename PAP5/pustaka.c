3c026f7e99356c2dd23f67ab3d59e643ef16cbc0
saipul <bayugrafit@gmail.com>
2018-03-31 17:35:18 +0700

    add new pustaka

--- PAP5/pustaka.c
+++ PAP5/pustaka.c
@@ -12,7 +12,47 @@ int i=0;
 }
 
 int is_inverse(int list1[], int list2[], int jml_data){
+    int i=0, j=0;
+    while(i<jml_data){
+
+        i++;
+    }
+}
+
+void min_max(int data[], int jml_data, int *min, int *max){
     int i=0;
+    *min=data[0];
+    *max=data[0];
+    while(i<jml_data){
+        if(data[i]<*min){
+            *min=data[i];
+        }else if(data[i]>*max){
+            *max=data[i];
+        }
+        i++;
+    }
+}
+
+int min_max_gap(int data[], int jml_data){
+    int min, max;
+    min_max(data,6,&min,&max);
+    return max-min;
+}
+
+int at_least(char huruf, int jml, char teks[]){
+    int i=0, hitung=0;
+    strcpy(temp, teks);
+    while(temp[i]!='\0'){
+        if(temp[1]==huruf){
+            hitung++;
+        }
+        i++;
+    }
+    if(hitung==jml){
+        return 'TRUE';
+    }else{
+        return 'FALSE';
+    }
 }
 
 void cetakarray(int data[],int panjang){

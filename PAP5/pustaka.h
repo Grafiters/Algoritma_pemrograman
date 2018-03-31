4d6da0a090e67a10a3454df6c822ba26c89f112c
saipul <bayugrafit@gmail.com>
2018-03-31 17:36:03 +0700

    add new header

--- PAP5/pustaka.h
+++ PAP5/pustaka.h
@@ -6,7 +6,11 @@
 
 int sequent_search(int angka, int data[], int jml_data);
 int is_inverse(int list1[], int list2[], int jml_data);
+int min_max_gap(int data[], int jml_data);
+int at_least(char huruf, int jml, char teks[]);
+char temp[];
 
+void min_max(int data[], int jml_data, int *min, int *max);
 void cetakarray(int data[],int panjang);
 
 #endif // PUSTAKA_H_INCLUDED

2443e5f6c859ca31439ab3c103cd13019e148576
saipul <bayugrafit@gmail.com>
2018-04-06 19:04:47 +0700

    add new header

--- PAP6/pustaka.h
+++ PAP6/pustaka.h
@@ -4,5 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int binarysearch(int angka, int data[], int jml_data);
+int search_word(char whord[], char text);
 
+void inputarray(int data[], int jml_data);
+void cetakarray(int data[], int jml_data);
 #endif // PUSTAKA_H_INCLUDED

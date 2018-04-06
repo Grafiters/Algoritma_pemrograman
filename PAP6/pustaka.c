fb8a2d4364513065ea2d311c9406e1608e10f7e9
saipul <bayugrafit@gmail.com>
2018-04-06 19:03:29 +0700

    add function

--- PAP6/pustaka.c
+++ PAP6/pustaka.c
@@ -1,1 +1,35 @@
 #include "pustaka.h"
+
+int binarysearch(int angka, int data[], int jml_data){
+    int i=0;
+    int min=0, max=jml_data-1, tengah;
+    int temu=-1;
+    while(temu==-1 && i<jml_data){
+        tengah=(min+max)/2;
+        if(data[tengah]==angka){
+            temu=tengah;
+        }else if(data[tengah]<angka){
+            min=tengah+1;
+        }else{
+            max=tengah-1;
+        }
+        i++;
+    }
+    return temu;
+}
+
+void inputarray(int data[], int jml_data){
+    int i=0;
+    while(i<jml_data){
+        printf("Input array ke-%d = ", i); scanf("%d", &data[i]);
+        i++;
+    }
+}
+
+void cetakarray(int data[], int jml_data){
+    int i=0;
+    while(i<jml_data){
+        printf("%d ", data[i]);
+        i++;
+    }
+}

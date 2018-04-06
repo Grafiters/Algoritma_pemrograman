792ada96c7b19771c09ccda6d8855063c629a890
saipul <bayugrafit@gmail.com>
2018-04-07 02:05:43 +0700

    add new comment

--- PAP6/main.c
+++ PAP6/main.c
@@ -1,18 +1,31 @@
 #include "pustaka.h"
 
 int data[];
-int jml_data, angka, hasil;
+int jml_data, angka, hasil, hasil1;
+char whord[], text[];
 
 int main()
 {
     printf("Inputkan jumlah data array = "); scanf("%d", &jml_data);
     int data[jml_data];
     inputarray(data,jml_data);
-    puts("");
+    puts("\n");
     cetakarray(data,jml_data);
     puts("");
     printf("Input angka yang dicari = "); scanf("%d", &angka);
     hasil=binarysearch(angka,data,jml_data);
     printf("apakah angka %d ada pada array tesebut %s", angka,binarysearch(angka,data,jml_data)?"TRUE":"FALSE");
+    puts("\n");
+    printf("Masukkan kalimat = "); scanf("%s", &text);
+    printf("%s\n", text);
+    printf("Masukkan kata yang ingin anda cari = "); scanf("%s", &whord);
+    hasil=search_word(whord,text);
+    if(hasil==1){
+        hasil1="TRUE";
+    }else{
+        hasil1="FALSE";
+    }
+    printf("%s", hasil1);
+
     return 0;
 }

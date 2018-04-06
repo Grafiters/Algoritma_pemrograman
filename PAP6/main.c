71aa1c5bf98ca8e7e8a3c178e32d832b52b659c2
saipul <bayugrafit@gmail.com>
2018-04-06 19:03:11 +0700

    add call function

--- PAP6/main.c
+++ PAP6/main.c
@@ -1,7 +1,18 @@
 #include "pustaka.h"
 
+int data[];
+int jml_data, angka, hasil;
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("Inputkan jumlah data array = "); scanf("%d", &jml_data);
+    int data[jml_data];
+    inputarray(data,jml_data);
+    puts("");
+    cetakarray(data,jml_data);
+    puts("");
+    printf("Input angka yang dicari = "); scanf("%d", &angka);
+    hasil=binarysearch(angka,data,jml_data);
+    printf("apakah angka %d ada pada array tesebut %s", angka,binarysearch(angka,data,jml_data)?"TRUE":"FALSE");
     return 0;
 }

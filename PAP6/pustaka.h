dcf906cfc3e80ef31b884af82864408b0238b0bc
saipul <bayugrafit@gmail.com>
2018-04-09 21:57:44 +0700

    add new pustaka

--- PAP6/pustaka.h
+++ PAP6/pustaka.h
@@ -4,9 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+    Nama     : Bayu Grafit Nur Alfian
+    NIM      : A11.2017.10284
+    Kelompok : 4207
+    Tugas PAP 5 (created, read, and update on linux)
+*/
+
 int binarysearch(int angka, int data[], int jml_data);
 int search_word(char whord[], char text[]);
+int is_anagram(char text1[], char text2[]);
 
+void frequency(char text[], int jml[]);
+void BubbleShort(int data[], int jml_data);
 void inputarray(int data[], int jml_data);
 void cetakarray(int data[], int jml_data);
+
+int jumlah1[];
+int jumlah2[];
 #endif // PUSTAKA_H_INCLUDED

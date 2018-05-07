022fbfdebd1f507b8be5a137fde4cca8de7aaaaf
Grafiters <grafiters@localhost.localdomain>
2018-05-07 23:25:11 +0700

    add new pustaka

--- PAP7/pustaka.h
+++ PAP7/pustaka.h
@@ -4,8 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add_rec(int num1, int num2);
+/**
+   Nama     = Bayu Grafit Nur Alfian
+   NIM      = A11.2017.10284
+   Kelompok = A11.4207
+   Created and edited on code block linux ubuntu budgie
+*/
 
-int a, b;
+int add_rec(int num1, int num2);
+int sub_rec(int num3, int num4);
+int mul_rect(int num1,int num2);
+int div_rec(int num1, int num2);
+int pow_rec(int num1, int num2);
+int factorial(int number);
+void fibonacci(int number);
+int sum_traversal(int data[], int cur);
+
+int a, b, c, d, e, f, g, h, i, j;
+
+void cetak(int n);
+int fibo(int n1, int n2,int i, int n);
 
 #endif // PUSTAKA_H_INCLUDED

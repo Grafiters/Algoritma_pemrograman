1564b6d57cfa1fb1e6574bf710ba8273e7e5e4d7
rizkimufti <grafiters@localhost.localdomain>
2018-05-15 21:10:51 +0700

    add new funtionc

--- PAP8/main.c
+++ PAP8/main.c
@@ -4,7 +4,7 @@ int a, b, hasil;
 int c, d, hail1;
 int hasil2;
 
-int data1[]={12, 15, 1, 23, 44, 21, 30};
+int data1[]={12, 15, 23, 44, 21, 30};
 int main()
 {
     printf("masukan angka pertama = "); scanf("%d", &a);
@@ -19,7 +19,17 @@ int main()
     printf("nilai terbesar adalah %d ", hail1);
 
     puts("\n");
-    hasil2=rec_max(data1,0,7);
+    hasil2=rec_max(data1,0,6);
     printf("nilai terbesar adalah %d", hasil2);
+
+    puts("\n");
+    printf("nilai terkecil adalah %d ", rec_min(data1,0,6));
+
+    puts("\n");
+    rec_print_all(data1,0,6);
+
+    puts("\n");
+    printf("mean dari data diatas adalah %d ", rec_mean(data1,0,6));
+
     return 0;
 }

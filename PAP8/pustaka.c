6466650d64383f8bf1c796016d7d49854c0cdef8
rizkimufti <grafiters@localhost.localdomain>
2018-05-16 22:13:51 +0700

    updated function

--- PAP8/pustaka.c
+++ PAP8/pustaka.c
@@ -1,5 +1,12 @@
 #include "pustaka.h"
 
+/*
+    Nama     = Bayu Grafit Nur Alfian
+    Kelompok = A11.4207
+    NIM      = A11.2017.10284
+    this file created, updated, and saved on code blocks linux
+*/
+
 int min2(int num1, int num2){
     int nilai;
     if(num1<num2){
@@ -30,7 +37,7 @@ int rec_min(int data[], int cur, int arrSize){
     if(cur==arrSize-1){
         return data[cur];
     }else{
-        return min2(data[cur], rec_max(data,cur+1,arrSize));
+        return min2(data[cur], rec_min(data,cur+1,arrSize));
     }
 }
 
@@ -44,15 +51,35 @@ void rec_print_all(int data[], int cur, int arrSize){
 }
 
 float rec_mean(int data[], int cur, int arrSize){
-    int hasil;
-    if(cur==arrSize){
-        return hasil/arrSize;
+    if(cur==0){
+        return ((data[cur]+rec_mean(data,cur+1,arrSize))/arrSize);
+    }else if(cur==arrSize-1){
+        return data[cur];
     }else{
-        return hasil += data[cur], rec_mean(data,cur+1,arrSize);
+        return (data[cur] + rec_mean(data,cur+1,arrSize));
     }
 }
 
+int balik(int *num1, int *num2){
+    int temp=*num1;
+    *num1=*num2;
+    *num2=temp;
+}
+
 void rec_selection_sort(int data[], int arrSize){
-    int index, temp;
-    if()
+    if (arrSize==0){
+        return data[arrSize-1];
+    }else{
+        int pos= inner_rec(data,arrSize-1,arrSize);
+        balik(&data[pos],&data[arrSize-1]);
+        return rec_selection_sort(data,arrSize-1);
+    }
+}
+
+int inner_rec(int data[], int cur, int arrSize){
+    if (data[arrSize-1]<data[cur]){
+        return cur= arrSize;
+    }else{
+        return inner_rec(data,cur,arrSize-1);
+    }
 }

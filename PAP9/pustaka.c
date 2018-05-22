63135cc1fc90d830a5046c2b604374c1e7569c73
rizkimufti <bayugrafit@gmail.com>
2018-05-23 00:09:19 +0700

    edited function

--- PAP9/pustaka.c
+++ PAP9/pustaka.c
@@ -1,5 +1,12 @@
 #include "pustaka.h"
 
+/*
+    Nama     = Bayu Grafit Nur Alfian
+    Kelompok = A11.4207
+    NIM      = A11.2017.10284
+    this file created, updated, and saved on code blocks linux
+*/
+
 pecahan makePecahan(int pemb, int peny){
     pecahan anyar;
     anyar.pembilang = pemb;
@@ -53,17 +60,39 @@ pecahan divPecahan(pecahan p1, pecahan p2){
 }
 
 float desimalPecahan(pecahan P){
+    float num1, num2;
+    num1=P.pembilang;
+    num2=P.penyebut;
 
+    return num1/num2;
 }
 
 int isEqual(pecahan p1, pecahan p2){
-
+    if(p1.pembilang/p2.pembilang==0 || p2.pembilang/p1.pembilang==0){
+        if(p1.penyebut/p2.penyebut==0 || p2.penyebut/p1.penyebut==0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }else{
+        return 0;
+    }
 }
 
 int isBigger(pecahan p1, pecahan p2){
-
+    float num1=desimalPecahan(p1), num2=desimalPecahan(p2);
+    if(num1>num2){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 int isSmall(pecahan p1, pecahan p2){
-
+    float num1=desimalPecahan(p1), num2=desimalPecahan(p2);
+    if(num1<num2){
+        return 1;
+    }else{
+        return 0;
+    }
 }
